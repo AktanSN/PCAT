6a0be20aea5bf79d7a9653ebf090199b7277c4ce
Aktan Sanhal <aktansanhal@gmail.com>
2023-02-19 22:12:04 +0300

    'front-end'

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
-const jsonSend =  { id: 1, title: "Blog title", description: "Blog description" };
 
-app.get('/', (req, res) => {
-    res.send(jsonSend);
+
+app.use(express.static('public'))
+
+app.get('/' , (req,res) =>{
+    res.sendFile(path.resolve(__dirname, './temp/index.html'));
 })
 
-const port = 3001
+
+
+
+const port = 3001;
 
 app.listen(port, () =>{
-    console.log(port)
+    console.log("listening on : "+ port)
 })
+
